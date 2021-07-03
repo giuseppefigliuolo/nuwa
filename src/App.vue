8ea63723215a37c1b109e13053ded552fc4cf2b1
Giuseppe Figliuolo <giuseppe.figliuolo@mono-grid.com>
2021-07-03 16:21:15 +0200

    navbar + images assets added

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,51 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
+    <nav-bar />
   </v-app>
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
-
+  components: { NavBar },
   data: () => ({
     //
-  }),
-};
+  })
+}
 </script>
+
+<style lang="scss">
+.v-application--wrap {
+  background-color: #212021;
+  color: white;
+}
+
+li {
+  list-style-type: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+h2,
+h3,
+h4,
+h5 {
+  font-family: 'Orbitron', sans-serif !important;
+}
+
+p {
+  font-family: 'Chakra Petch', sans-serif;
+}
+
+.h-font {
+  font-family: 'Orbitron', sans-serif !important;
+}
+
+.p-font {
+  font-family: 'Chakra Petch', sans-serif;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav>
+    <div class="logo">
+      <img :src="require('../assets/logo.svg')" alt="" />
+    </div>
+    <div class="menu">
+      <ul>
+        <li v-for="item in menuItems" :key="item" class="h-font">{{ item }}</li>
+      </ul>
+    </div>
+    <div class="countdown d-flex justify-center">
+      <p class="h-font">6mesi 20giorni 11h 32min 10s</p>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuItems: ['Capsula', 'Marte', 'Contact']
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  padding: 0.8rem 2rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+
+.menu ul {
+  display: flex;
+  justify-content: space-evenly;
+  font-size: 0.9rem;
+}
+</style>
