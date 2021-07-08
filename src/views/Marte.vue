63d3afa6aee8d742a302123bc3344f0082ffb6a3
aury61100 <57524709+aury61100@users.noreply.github.com>
2021-07-08 17:23:49 +0200

    Pagina Marte

---
+++ src/views/Marte.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+      <hero-section />
+      <illustration-section
+        :titleIllustration="titleIllustration"
+      />
+      
+  </main>
+</template>
+
+<script>
+import HeroSection from '../components/HeroSection.vue'
+import IllustrationSection from '../components/IllustrationSection.vue'
+
+export default {
+  name: 'Marte',
+  data() {
+    return {
+      titleIllustration:'Scheda tecnica di un viaggiatore Nüwa '
+    }
+  },
+  components: {
+    HeroSection,
+    IllustrationSection
+  }
+}
+</script>
+
+<style>
+
+</style>
