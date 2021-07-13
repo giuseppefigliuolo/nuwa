3ff6edb8f3ed22168a2381eae58b0eb1f4326fd7
aury61100 <57524709+aury61100@users.noreply.github.com>
2021-07-13 10:28:35 +0200

    capsule title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <intro />
     </template>
     <template v-else>
-      <hero-section title="Le capsule Nuwa nel dettaglio"/>
+      <hero-section title="Scopri le capsule NUWA"/>
       <illustration-section
         :titleIllustration="titleIllustration"
         img="capsule-illustration.png"
